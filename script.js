8b600f9b9f540a97a273f350efffacc034dd4c3a
Yudhi <baskyudh@gmail.com>
2025-02-17 13:34:14 +0700

    Initialized project

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// const d = document.querySelector('');
+// const a = document.querySelector('');
+// const b = document.querySelector('');
+// const c = document.querySelector('');
